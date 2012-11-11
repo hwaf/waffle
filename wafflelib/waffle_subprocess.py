928202efc3db385cba5c1e6acd6f6c8e7827a90b
Sebastien Binet <binet@cern.ch>
2012-11-11 23:46:57 +0100

    subprocess: correctly relay subprocess content

--- wafflelib/waffle_subprocess.py
+++ wafflelib/waffle_subprocess.py
@@ -1,4 +1,4 @@
 # redirection layer to subprocess to handle py2/py3
-try: import waflib.extras.waffle_py2_subprocess as subprocess
+try: from waflib.extras.waffle_py2_subprocess import *
 # py-3
-except SyntaxError: import subprocess
+except SyntaxError: from subprocess import *
